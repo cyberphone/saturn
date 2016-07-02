6312626eaea948907c89aeee5c12f0ac0499dcc5
Anders Rundgren <anders.rundgren.net@gmail.com>
2016-07-02 12:03:08 +0200

    Removed JSON crypto test (it is in core now) but left the Node.js test data.

--- resources/src/org/webpki/saturn/resources/CryptoTesting.java
+++ resources/src/org/webpki/saturn/resources/CryptoTesting.java
@@ -23,19 +23,12 @@ import java.math.BigInteger;
 
 import java.security.KeyFactory;
 import java.security.KeyPair;
-import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-
-import java.security.interfaces.ECPublicKey;
 
 import java.security.spec.ECPoint;
 import java.security.spec.ECPrivateKeySpec;
 import java.security.spec.ECPublicKeySpec;
-
-import java.util.Vector;
-
-import javax.crypto.KeyAgreement;
 
 import org.webpki.asn1.ASN1OctetString;
 import org.webpki.asn1.ASN1Sequence;
@@ -47,20 +40,17 @@ import org.webpki.asn1.ParseUtil;
 import org.webpki.crypto.AlgorithmPreferences;
 import org.webpki.crypto.CustomCryptoProvider;
 import org.webpki.crypto.KeyAlgorithms;
-import org.webpki.crypto.DecryptionKeyHolder;
 
 import org.webpki.json.JSONObjectReader;
-import org.webpki.json.JSONOutputFormats;
 import org.webpki.json.JSONParser;
-import org.webpki.json.JSONEncryption;
-
-import org.webpki.json.encryption.EncryptionCore;
 
 import org.webpki.util.ArrayUtil;
 import org.webpki.util.Base64;
-import org.webpki.util.Base64URL;
-import org.webpki.util.DebugFormatter;
 
+/**
+ * Create test data for the Node.js encryption implementation
+ *
+ */
 public class CryptoTesting {
     
     static final String ECDH_RESULT_WITHOUT_KDF = "SzFxLgluXyC07Pl5D9jMfIt-LIrZC9qByyJPYsDnuaY";
@@ -131,116 +121,8 @@ public class CryptoTesting {
             System.exit(3);
         }
         CustomCryptoProvider.forcedLoad(true);
-        System.out.println("Content EncryptionCore");
-        byte[] k = DebugFormatter.getByteArrayFromHex("000102030405060708090a0b0c0d0e0f" +
-                                                      "101112131415161718191a1b1c1d1e1f");
-
-        byte[] p = DebugFormatter.getByteArrayFromHex("41206369706865722073797374656d20" +
-                                                      "6d757374206e6f742062652072657175" +
-                                                      "6972656420746f206265207365637265" +
-                                                      "742c20616e64206974206d7573742062" +
-                                                      "652061626c6520746f2066616c6c2069" +
-                                                      "6e746f207468652068616e6473206f66" +
-                                                      "2074686520656e656d7920776974686f" +
-                                                      "757420696e636f6e76656e69656e6365");
-
-        byte[] iv = DebugFormatter.getByteArrayFromHex("1af38c2dc2b96ffdd86694092341bc04");
-
-        byte[] a = DebugFormatter.getByteArrayFromHex("546865207365636f6e64207072696e63" +
-                                                      "69706c65206f66204175677573746520" +
-                                                      "4b6572636b686f666673");
-
-        byte[] e = DebugFormatter.getByteArrayFromHex("c80edfa32ddf39d5ef00c0b468834279" +
-                                                      "a2e46a1b8049f792f76bfe54b903a9c9" +
-                                                      "a94ac9b47ad2655c5f10f9aef71427e2" +
-                                                      "fc6f9b3f399a221489f16362c7032336" +
-                                                      "09d45ac69864e3321cf82935ac4096c8" +
-                                                      "6e133314c54019e8ca7980dfa4b9cf1b" +
-                                                      "384c486f3a54c51078158ee5d79de59f" +
-                                                      "bd34d848b3d69550a67646344427ade5" +
-                                                      "4b8851ffb598f7f80074b9473c82e2db");
-
-        byte[] t = DebugFormatter.getByteArrayFromHex("652c3fa36b0a7c5b3219fab3a30bc1c4");
-
-        byte[] pout = EncryptionCore.contentDecryption(JSONEncryption.JOSE_A128CBC_HS256_ALG_ID,
-                                                       k,
-                                                       e,
-                                                       iv,
-                                                       a,
-                                                       t);
-        if (!ArrayUtil.compare(p, pout)) {
-            throw new IOException ("pout 1");
-        }
-        EncryptionCore.AuthEncResult aer = EncryptionCore.contentEncryption(JSONEncryption.JOSE_A128CBC_HS256_ALG_ID,
-                                                                            k,
-                                                                            p,
-                                                                            a);
-        pout = EncryptionCore.contentDecryption(JSONEncryption.JOSE_A128CBC_HS256_ALG_ID,
-                                                k,
-                                                aer.getCipherText(),
-                                                aer.getIv(),
-                                                a,
-                                                aer.getTag());
-        if (!ArrayUtil.compare(p, pout)) {
-            throw new IOException ("pout 2");
-        }
-        byte[] dataEncryptionKey = EncryptionCore.generateDataEncryptionKey(JSONEncryption.JOSE_A128CBC_HS256_ALG_ID);
-        JSONObjectReader json = JSONParser.parse(aliceKey);
-        String encrec = JSONEncryption.encode(json.serializeJSONObject(JSONOutputFormats.NORMALIZED),
-                                              JSONEncryption.JOSE_A128CBC_HS256_ALG_ID,
-                                              dataEncryptionKey).toString();
-        if (!JSONParser.parse(JSONEncryption.parse(JSONParser.parse(encrec), true).getDecryptedData(dataEncryptionKey)).toString().equals(json.toString())) {
-           throw new IOException("Symmetric");
-        }
-        System.out.println("ECDH begin");
         KeyPair bob = getKeyPair(bobKey);
         KeyPair alice = getKeyPair(aliceKey);
-        if (!Base64URL.encode(EncryptionCore.receiverKeyAgreement(JSONEncryption.JOSE_ECDH_ES_ALG_ID,
-                                                                  JSONEncryption.JOSE_A128CBC_HS256_ALG_ID,
-                                                                  (ECPublicKey) bob.getPublic(),
-                                                                  alice.getPrivate())).equals(ECDH_RESULT_WITH_KDF)) {
-            throw new IOException("Bad ECDH");
-        }
-        EncryptionCore.EcdhSenderResult ecdhRes = 
-            EncryptionCore.senderKeyAgreement(JSONEncryption.JOSE_ECDH_ES_ALG_ID,
-                                              JSONEncryption.JOSE_A128CBC_HS256_ALG_ID,
-                                              alice.getPublic());
-        if (!ArrayUtil.compare(ecdhRes.getSharedSecret(),
-            EncryptionCore.receiverKeyAgreement(JSONEncryption.JOSE_ECDH_ES_ALG_ID,
-                                                JSONEncryption.JOSE_A128CBC_HS256_ALG_ID,
-                                                ecdhRes.getEphemeralKey(),
-                                                alice.getPrivate()))) {
-            throw new IOException("Bad ECDH");
-        }
-        KeyPairGenerator mallet = KeyPairGenerator.getInstance("RSA", "BC");
-        mallet.initialize(2048);
-        KeyPair malletKeys = mallet.generateKeyPair();
-        Vector<DecryptionKeyHolder> decryptionKeys = new Vector<DecryptionKeyHolder>();
-        decryptionKeys.add(new DecryptionKeyHolder(alice.getPublic(), alice.getPrivate(), JSONEncryption.JOSE_ECDH_ES_ALG_ID));
-        decryptionKeys.add(new DecryptionKeyHolder(bob.getPublic(), bob.getPrivate(), JSONEncryption.JOSE_ECDH_ES_ALG_ID));
-        decryptionKeys.add(new DecryptionKeyHolder(malletKeys.getPublic(), malletKeys.getPrivate(), JSONEncryption.JOSE_RSA_OAEP_256_ALG_ID));
-
-        JSONObjectReader unEncJson = JSONParser.parse("{\"hi\":\"\\u20ac\\u00e5\\u00f6k\"}");
-        String encJson = JSONEncryption.encode(unEncJson.serializeJSONObject(JSONOutputFormats.NORMALIZED),
-                                               JSONEncryption.JOSE_A128CBC_HS256_ALG_ID,
-                                               bob.getPublic(),
-                                               JSONEncryption.JOSE_ECDH_ES_ALG_ID).toString();
-        if (!unEncJson.toString().equals(JSONParser.parse(JSONEncryption.parse(JSONParser.parse(encJson), false).getDecryptedData(decryptionKeys)).toString())) {
-            throw new IOException("Bad JOSE ECDH");
-        }
-        encJson = JSONEncryption.encode(unEncJson.serializeJSONObject(JSONOutputFormats.NORMALIZED),
-                                        JSONEncryption.JOSE_A128CBC_HS256_ALG_ID,
-                                        malletKeys.getPublic(),
-                                        JSONEncryption.JOSE_RSA_OAEP_256_ALG_ID).toString();
-        if (!unEncJson.toString().equals(JSONParser.parse(JSONEncryption.parse(JSONParser.parse(encJson), false).getDecryptedData(decryptionKeys)).toString())) {
-            throw new IOException("Bad JOSE ECDH");
-        }
-        KeyAgreement keyAgreement = KeyAgreement.getInstance("ECDH", "BC");
-        keyAgreement.init(alice.getPrivate());
-        keyAgreement.doPhase(bob.getPublic(), true);
-        if (!Base64URL.encode(keyAgreement.generateSecret()).equals(ECDH_RESULT_WITHOUT_KDF)) {
-            throw new IOException("Bad ECDH");
-        }
         js.append("// ECDH test data\n\n" +
                   "const ECDH_RESULT_WITH_KDF    = '" + ECDH_RESULT_WITH_KDF + "';\n" +
                   "const ECDH_RESULT_WITHOUT_KDF = '" + ECDH_RESULT_WITHOUT_KDF + "';\n\n");
@@ -248,6 +130,6 @@ public class CryptoTesting {
                                                    alice.getPrivate().getEncoded()));
         createPEM("PUBLIC", bob.getPublic().getEncoded());
         ArrayUtil.writeFile(args[0], js.toString().getBytes("UTF-8"));
-        System.out.println("ECDH success");
+        System.out.println(js.toString());
     }
 }
