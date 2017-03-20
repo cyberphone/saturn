4db4be9aefc3874d784b022b39453eab65906c10
Anders Rundgren <anders.rundgren.net@gmail.com>
2017-03-20 07:52:03 +0100

    Added property listing to browser-invoked authority objects

--- resources/common/org/webpki/saturn/common/AuthorityBaseServlet.java
+++ resources/common/org/webpki/saturn/common/AuthorityBaseServlet.java
@@ -73,6 +73,7 @@ public abstract class AuthorityBaseServlet extends HttpServlet implements BasePr
                             "<i>live object</i> is normally requested by service providers for looking up partner core data. In this case " +
                             "the requester seems to be a browser which is why a &quot;pretty-printed&quot; HTML page is returned instead of raw JSON.")
                     .append(isProvider() ? "" : "</td></tr><tr><td style=\"padding-bottom:8pt\">" +
+                            "This particular (payee) object was issued by the provider specified by the " +
                             keyWord(PROVIDER_AUTHORITY_URL_JSON) +
                             " property: <a href=\"" +
                             rd.getString(PROVIDER_AUTHORITY_URL_JSON) + "\" target=\"_blank\">" + 
