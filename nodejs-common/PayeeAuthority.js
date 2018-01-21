2c316a67aa098b2613b86828e4827d26a685093a
Anders Rundgren <anders.rundgren.net@gmail.com>
2018-01-21 12:12:05 +0100

    2018

--- nodejs-common/PayeeAuthority.js
+++ nodejs-common/PayeeAuthority.js
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2006-2016 WebPKI.org (http://webpki.org).
+ *  Copyright 2015-2018 WebPKI.org (http://webpki.org).
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
