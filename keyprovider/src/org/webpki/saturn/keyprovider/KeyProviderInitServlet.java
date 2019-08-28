c88ad48d855bda68cdad778a2ca10a7b2e256e88
Anders Rundgren <anders.rundgren.net@gmail.com>
2019-08-28 18:54:09 +0200

    Improved W3C PaymentRequest code

--- keyprovider/src/org/webpki/saturn/keyprovider/KeyProviderInitServlet.java
+++ keyprovider/src/org/webpki/saturn/keyprovider/KeyProviderInitServlet.java
@@ -279,6 +279,9 @@ public class KeyProviderInitServlet extends HttpServlet {
             "        const payRequest = new PaymentRequest(supportedInstruments, details);\n" +
             "        const payResponse = await payRequest.show();\n" +
             "        payResponse.complete('success');\n" +
+            // Note that success does not necessarily mean that the enrollment succeeded,
+            // it just means that result is a url to be redirected to.
+            "        document.location.href = payResponse.details.goto;\n" +
             "      }\n" +
             "    } catch (err) {\n" +
             "      console.error(err);\n" +
