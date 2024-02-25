e87c19758d480683fbcccc69ef51c6281fa549ab
Anders Rundgren <anders.rundgren.net@gmail.com>
2024-02-25 18:06:00 +0100

    More Chrome/PaymentRequest updates

--- w3cpay/src/org/webpki/saturn/w3c/manifest/PaymentAppMethodService.java
+++ w3cpay/src/org/webpki/saturn/w3c/manifest/PaymentAppMethodService.java
@@ -73,8 +73,8 @@ public class PaymentAppMethodService extends InitPropertyReader implements Servl
             byte[] certificate = getBinary(SIGNER_CERTIFICATE);
 
             JSONObjectWriter temp = new JSONObjectWriter();
-//            temp.setArray("default_applications")
-//                .setString(getPropertyString(HOST_PATH));
+// Removed due to https://issues.chromium.org/issues/325593948
+//            temp.setArray("default_applications").setString(getPropertyString(HOST_PATH));
             JSONObjectWriter oneApp = new JSONObjectWriter();
             oneApp.setString("platform", "play")
                   .setString("id", MobileProxyParameters.ANDROID_PACKAGE_NAME)
